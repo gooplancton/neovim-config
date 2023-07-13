202de88b1066274e288927cc17dddedf0cbcee2d
gooplancton <gooplancton@outlook.com>
2023-07-13 16:55:13 +0200

    added plugins

--- init.lua
+++ init.lua
@@ -234,10 +234,10 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animate_in_insert_mode = true
 end
 
--- Tab is two spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+-- Tab is four spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 -- Set highlight on search
 vim.o.hlsearch = false
